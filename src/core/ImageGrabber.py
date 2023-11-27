0d3587d6229fd9aee6edb488768cc05d2d1eba39
khperera <126294394+ltnflash@users.noreply.github.com>
2023-11-27 14:50:16 -0500

    Update

---
+++ src/core/ImageGrabber.py
@@ -0,0 +1,15 @@
+class imageGrabber:
+    def __init__(self):
+
+
+        self.imageLocation = ""
+
+
+    #user command to give a location, then give an image back
+    def returnImage(self,location):
+        self.imageLocation = location
+        return self.openImage()
+
+    #the image reading function
+    def openImage(self):
+        return cv2.imread(self.imageLocation,1)
---
+++ src/core/ImageHolder.py
@@ -0,0 +1,27 @@
+#a class that holds an image and information about an image.
+class ImageHolder:
+
+    def __init__(self):
+        self.img = np.zeros((1,1,3), dtype=np.uint8)
+        self.imgInfo = {"ImageType" : "None", "Name" : "None"}
+
+    #deletes the image and information about the image
+    def __del__(self):
+        del self.img
+        del self.imgInfo
+
+    #takes in a numpy array that represents an image
+    def storeImage(self, img):
+        self.img = img
+
+    #stores the image info
+    def storeImageInfo(self,imgInfo):
+        self.imgInfo = imgInfo
+
+    #returns the image
+    def returnImage(self):
+        return self.img
+
+    #returns the information about an image
+    def returnImageInfo(self):
+        return self.imgInfo
